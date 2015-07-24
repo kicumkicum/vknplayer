7bc8f19c5156ea91c07cfc3a9470f65cf1ace448
kicumkicum <kicumkicum@gmail.com>
2015-07-25 00:46:57 +0600

    add draft wrapper yandex-music-api

---
+++ app/api/lib/yandex-music.js
@@ -0,0 +1,48 @@
+var Promise = require('promise');
+var YandexMusicApi = require('yandex-music-api');
+var events = require('events');
+var util = require('util');
+
+
+var YandexMusic = function() {
+	this.init({username: 'kicumkicum@yandex.ru', password: '11zhlm5a'}).then(function() {
+		// place code here
+	});
+
+};
+util.inherits(YandexMusic, YandexMusicApi);
+
+
+/**
+ * @param {string} query
+ * @param {YandexMusic.SearchOptions} options
+ */
+YandexMusic.prototype.search = function(query, options) {
+	return goog.base(this, 'search', query, options)
+		.then(function(response) {
+			aaa = response
+		});
+};
+
+
+/**
+ * @typedef {{
+ *      type: YandexMusic.SearchOptionsType,
+ *      page: number,
+ *      nocorrect: boolean
+ * }}
+ */
+YandexMusic.SearchOptions;
+
+
+/**
+ * @enum {string}
+ */
+YandexMusic.SearchOptionsType = {
+	ARTIST: 'artist',
+	ALBUM: 'album',
+	TRACK: 'track',
+	ALL: 'all'
+};
+
+module.exports = YandexMusic;
