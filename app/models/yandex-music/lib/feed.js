11c9fc6735baa97a0028e1b8d10222f1b12c9239
kicumkicum <kicumkicum@gmail.com>
2015-07-30 01:05:15 +0600

    Add feed-model

---
+++ app/models/yandex-music/lib/feed.js
@@ -0,0 +1,29 @@
+
+
+
+/**
+ * @param {*} data
+ * @constructor
+ */
+var Feed = function(data) {
+	data = data || {};
+
+	/** @type {boolean} */
+	this.canGetMoreEvents = data['canGetMoreEvents'];
+
+	/** @type {} */
+	this.days = data['days'];
+
+	/** @type {} */
+	// todo
+	this.headlines = data['headlines'];
+
+	/** @type {string} */
+	this.nextRevision = data['nextRevision'];
+
+	/** @type {string} */
+	this.today = data['today'];
+};
+
+
+module.exports = Feed;
