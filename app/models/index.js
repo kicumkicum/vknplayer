7cacb75d3ddbef3e2091038c0cf43da83cac504c
kicumkicum <kicumkicum@gmail.com>
2015-08-05 00:54:46 +0600

    Add abstract-model for vknp

--- app/models/index.js
+++ app/models/index.js
@@ -3,6 +3,7 @@ var models = {};
 
 module.exports = models;
 
+models.AbstractModel = require('./lib/abstract-model');
 models.Album = require('./lib/album');
 models.AudioTrack = require('./lib/audio-track');
 models.Config = require('./lib/config');
