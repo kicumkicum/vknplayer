bf4d4ca48043187a2fc48eb3210b4660194f6266
kicumkicum <kicumkicum@gmail.com>
2015-09-03 02:33:04 +0600

    Add base data-views

---
+++ app/data-view/lib/service-yandex-music.js
@@ -0,0 +1,19 @@
+var dataViews = require('../');
+
+
+
+/**
+ *
+ * @constructor
+ */
+var ServiceYandexMusic = function() {};
+goog.inherits(ServiceYandexMusic, dataViews.Abstract);
+
+
+/**
+ * @return {Promise.<Array>}
+ */
+ServiceYandexMusic.prototype.getChilds = function() {};
+
+
+module.exports = ServiceYandexMusic;
