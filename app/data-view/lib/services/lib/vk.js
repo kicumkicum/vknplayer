babd3382dcd9bccf92b1c3d9a62a817b3fc3b81e
kicumkicum <kicumkicum@gmail.com>
2015-09-04 02:30:22 +0600

    Remove surplus child vk-service-data-view

--- app/data-view/lib/services/lib/vk.js
+++ app/data-view/lib/services/lib/vk.js
@@ -16,15 +16,10 @@ goog.inherits(ServiceVK, dataViews.Abstract);
 ServiceVK.prototype.getChild = function() {
 	return new vknp.Promise(function(resolve, reject) {
 		resolve([
-			new dataViews.Bookmark,
 			new dataViews.Bookmarks,
-			new dataViews.Friend,
 			new dataViews.Friends,
-			new dataViews.Group,
 			new dataViews.Groups,
 			new dataViews.News,
-			new dataViews.NewsItem,
-			new dataViews.Playlist,
 			new dataViews.Playlists
 		]);
 	});
