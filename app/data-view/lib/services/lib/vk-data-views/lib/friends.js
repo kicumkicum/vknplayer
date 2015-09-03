eafd97834e0c93198d3f9ea3533b682e97c857cc
kicumkicum <kicumkicum@gmail.com>
2015-09-04 02:30:53 +0600

    Done friend-vk-data-view

--- app/data-view/lib/services/lib/vk-data-views/lib/friends.js
+++ app/data-view/lib/services/lib/vk-data-views/lib/friends.js
@@ -11,15 +11,25 @@ goog.inherits(Friends, dataViews.Abstract);
 
 
 /**
- * @return {Promise.<Array>}
+ * @return {Promise.<Array.<dataViews.Friend>>}
  */
-Friends.prototype.getChild = function() {};
+Friends.prototype.getChild = function() {
+	return app.api.vk
+		.getFriends(30)
+		.then(function(friends) {
+			return friends.map(function(friend) {
+				return new dataViews.Friend(friend);
+			});
+		});
+};
 
 
 /**
  * @return {string}
  */
-Friends.prototype.toString = function() {};
+Friends.prototype.toString = function() {
+	return 'Friends';
+};
 
 
 module.exports = Friends;
