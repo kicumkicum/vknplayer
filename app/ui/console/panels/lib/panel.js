a0f4a5aeb4932b1d48bbb2ab6810fb35773e01de
kicumkicum <kicumkicum@gmail.com>
2015-09-09 02:04:26 +0600

    Add universal panel for work with data-views

---
+++ app/ui/console/panels/lib/panel.js
@@ -0,0 +1,104 @@
+goog.provide('Panel');
+
+var BasePanel = require('./base-panel');
+var Node = require('../../lib/node');//todo for inherits. check it
+
+var blessed = require('blessed');
+
+
+
+/**
+ * @constructor
+ * @extends {BasePanel}
+ */
+var Panel = function(dataView) {
+	this._category = [];
+	this._dataView = dataView;
+	goog.base(this, {
+		mouse: true,
+		keys: true,
+		scrollable: true,
+		hidden: !true,
+
+		left: 0,
+		top:  2,
+		bottom: 4,
+		width: '50%',
+
+		border: {
+			type: 'line'
+		},
+
+		style: {
+			selected: {
+				fg: 'black'
+			}
+		}
+	});
+};
+goog.inherits(Panel, BasePanel);
+
+
+/**
+ * @param {} dataView
+ * @protected
+ */
+Panel.prototype.setDataView = function(dataView) {
+	this._dataView = dataView;
+	this._loadData();
+};
+
+
+/**
+ * @protected
+ */
+Panel.prototype._init = function() {
+	goog.base(this, '_init');
+	this._loadData();
+};
+
+
+/**
+ * @protected
+ */
+Panel.prototype._loadData = function() {
+	goog.base(this, '_loadData');
+
+	this._dataView.getChild().then(function(child) {
+		this._category = [].concat(child);
+		if (child.length && child[0] instanceof vknp.models.AudioTrack) {
+			app.ui.console._panels.mainPL.setContent(child);
+		} else {
+			this.setData(this._category);
+		}
+	}.bind(this));
+};
+
+
+/**
+ * @param {*} select
+ * @param {number} selectNumber
+ * @protected
+ */
+Panel.prototype._clickHandler = function(eventName, select, selectNumber) {
+	if (selectNumber === 0) {
+		this._back();
+		return;
+	}
+
+	var item = this._getDataItem(selectNumber);
+	if (!item) {
+		return;
+	}
+
+	this.setDataView(item);
+};
+
+
+/**
+ * @type {Array.<Panel.Category>}
+ */
+Panel.prototype._category;
+
+
+module.exports = Panel;
