77d4681d3442c17684c83aed8fef418fa2815b94
kicumkicum <kicumkicum@gmail.com>
2015-09-11 00:35:14 +0600

    Add data-view to global namespace

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 global.vknp = {};
 
 vknp.api = require('./api');
+vknp.dataViews = require('./data-view');
 vknp.UI = require('./ui');
 vknp.ui = vknp.UI.namespace;
 vknp.service = require('./service');
