096ed585bda908448de753646aa2e27c721e21c5
kicumkicum <kicumkicum@gmail.com>
2015-09-25 01:34:27 +0600

    Move create web-socket to web-ui

--- app/ui/web/lib/web.js
+++ app/ui/web/lib/web.js
@@ -1,7 +1,10 @@
 var BaseUI = require('../../base-ui');
+var WebSocketServer = require('ws').Server;
+var connect = require('connect');
 var path = require('path');
-var express = require('express');
-var http = require('http');
+
+
+
 /**
  * @inheritDoc
  * @constructor
@@ -10,19 +13,96 @@ var http = require('http');
 var Web = function(config, dataViews, api, player, playlist, historyManager) {
 	goog.base(this, config, dataViews, api, player, playlist, historyManager);
 
-	var app = express();
-
-	http.createServer(app)
-		.listen(1337, function() {
-			console.log('Server listening on: http://localhost:%s', 1337);
-		}.bind(this));
-
-	app.use('/', express.static(path.join(__dirname, '..')));
-
-	var ws = new vknp.service.WebSocket;
-
+	this._createWebSocket();
+	this._createWebServer();
 };
 goog.inherits(Web, BaseUI);
 
 
+/**
+ * @protected
+ */
+Web.prototype._createWebSocket = function() {
+	var socket = new WebSocketServer({port: 8081});
+	socket.on('connection', function(ws) {
+		ws.on('message', this._onMessage.bind(this));
+		this._ws = ws;
+	}.bind(this));
+};
+
+
+/**
+ * @protected
+ */
+Web.prototype._createWebServer = function() {
+	connect()
+		.use(connect.static(path.join(__dirname, '..')))
+		.listen(1337);
+};
+
+
+/**
+ * @param {Web.Message} stringMessage
+ * @protected
+ */
+Web.prototype._onMessage = function(stringMessage) {
+	var message = stringMessage;
+	switch (message) {
+		case 'play':
+			app.service.player.resume();
+			break;
+		case 'resume':
+			app.service.player.resume();
+			break;
+		case 'pause':
+			app.service.player.stop();
+			break;
+		case 'prev':
+			app.service.player.prev();
+			break;
+		case 'next':
+			app.service.player.next();
+			break;
+		case 'get-list':
+			var list = app.service.playListManager.getActivePlaylist().toArray();
+			this._ws.send(JSON.stringify(list));
+			break;
+		default:
+			var mes = JSON.parse(message);
+	}
+
+	if (mes && mes.play) {
+		var current = app.service.playListManager.getActivePlaylist().toArray().filter(function(item) {
+			return item.id === mes.play;
+		})[0];
+
+		app.service.playListManager.getActivePlaylist().select(current);
+		app.service.player.play(app.service.playListManager.getActivePlaylistId());
+	}
+};
+
+
+/**
+ * @enum {string}
+ */
+Web.MessageType = {
+	EVENT: 'event',
+	REQUEST: 'request',
+	RESPONSE: 'response'
+};
+
+
+/**
+ * @typedef {{
+ *     type: Web.MessageType,
+ *     name: string,
+ *     data: (*|undefined)
+ * }}
+ */
+Web.Message;
+
+
+/**
+ * @type {Web}
+ */
 module.exports = Web;
